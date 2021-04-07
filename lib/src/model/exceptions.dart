2a0e6fc6a019462389279973635926ee99767e44
Alqueraf <alqueraf@gmail.com>
2021-04-07 18:33:23 +0200

    Add toString implementation for exceptions

--- lib/src/model/exceptions.dart
+++ lib/src/model/exceptions.dart
@@ -5,6 +5,11 @@ class FailedToGetMinimumVersions implements AppVersioningException {
   final String? error;
 
   const FailedToGetMinimumVersions({this.error});
+
+  @override
+  String toString() {
+    return 'FailedToGetMinimumVersions{error: $error}';
+  }
 }
 
 /// Error getting current version from device
@@ -12,4 +17,9 @@ class FailedToGetCurrentVersion implements AppVersioningException {
   final String? error;
 
   const FailedToGetCurrentVersion({this.error});
+
+  @override
+  String toString() {
+    return 'FailedToGetCurrentVersion{error: $error}';
+  }
 }
